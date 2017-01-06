17f60a6ae20b0b7f4475de5eb1f8f3a210430c98
renshunwei <rsw1992@163.com>
2017-01-06 18:00:04 +0800

    jinxiuRefresh上下拉刷新组件

--- library/src/main/java/com/jinxiu/refresh/inter/RefreshStates.java
+++ library/src/main/java/com/jinxiu/refresh/inter/RefreshStates.java
@@ -3,21 +3,21 @@ package com.jinxiu.refresh.inter;
 /**
  * Created by apple on 17/1/5.
  */
-public enum PullStates {
+public enum RefreshStates {
 
     DEFAULT,//默认状态
 
-    DOWN_BEFORE,//下拉中，到达有效刷新距离前
+    DOWN_START,//下拉中，到达有效刷新距离前
     DOWN_AFTER,//下拉中，到达有效刷新距离后
     REFRESH_SCROLLING,//放手后，开始刷新前，回到刷新的位置中
     REFRESH_DOING,//正在刷新中
     REFRESH_COMPLETE_SCROLLING,//刷新完成后，回到默认状态中
     REFRESH_CANCEL_SCROLLING,//刷新取消后，回到默认状态中
 
-    UP_BEFORE,//上拉中，到达有效刷新距离前
+    UP_START,//上拉中，到达有效刷新距离前
     UP_AFTER,//上拉中，到达有效刷新距离后
-    LOADMORE_SCROLLING,//放手后，开始加载前，从手势位置回到加载的位置中
-    LOADMORE_DOING,//正在加载中
-    LOADMORE_COMPLETE_SCROLLING,//加载完成后，回到默认状态中
-    LOADMORE_CANCEL_SCROLLING,//加载取消后，回到默认状态中
+    LOAD_MORE_SCROLLING,//放手后，开始加载前，从手势位置回到加载的位置中
+    LOAD_MORE_DOING,//正在加载中
+    LOAD_MORE_COMPLETE_SCROLLING,//加载完成后，回到默认状态中
+    LOAD_MORE_CANCEL_SCROLLING,//加载取消后，回到默认状态中
 }
---
+++ library/src/main/java/com/jinxiu/refresh/inter/pullHead.java
@@ -0,0 +1,38 @@
+package com.jinxiu.refresh.inter;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.FrameLayout;
+
+/**
+ * Created by apple on 17/1/5.
+ * 下拉刷新接口
+ */
+public abstract class PullHead extends FrameLayout{
+
+    public PullHead(Context context) {
+        super(context);
+    }
+
+    public PullHead(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    //开始下拉刷新
+    public abstract void onDownBefore(int scrollY);
+
+    //松开刷新(下拉中，到达有效刷新距离后)
+    public abstract  void onDownAfter(int scrollY);
+
+    //准备刷新(从松手后的位置滚动到刷新的位置)
+    public abstract void onRefreshScrolling(int scrollY);
+
+    //正在刷新……
+    public abstract void onRefreshDoing(int scrollY);
+
+    //刷新完成
+    public abstract void onRefreshCompleteScrolling(int scrollY, boolean isRefreshSuccess);
+
+    //刷新取消后，回到默认状态中（没有超过有效的下拉距离）
+    public abstract void onRefreshCancelScrolling(int scrollY);
+}
